0f5be8d685190a4612eb20ed3ae2b681d94b38d6
antonio.neves <antonio.neves@ifood.com.br>
2026-06-14 15:35:36 -0300

    ​feat: adiciona modulo __init__ com metadados do pacote

---
+++ src/codetwin_analayzer/__init__.py
@@ -0,0 +1,10 @@
+"""
+CodeTwin Analyzer
+
+Uma ferramenta para análise de código e detecção de clones.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Antônio Neves, Bernardo Dutra, João Lucas, Raphael Aroldo"
+
+__all__ = []
